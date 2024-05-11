0b2bbccde46a7c88b5081a5b317b690f244f1482
John <24556317+Johnnycyan@users.noreply.github.com>
2024-05-11 03:02:42 -0400

    Added rate limit and reponses to such

--- main.go
+++ main.go
@@ -4,12 +4,33 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 const baseURL = "https://discord.com/api/webhooks"
+
+var lastRequest time.Time
+var rateLimit = 30 * time.Second
+var rateLimitResponse = []string{
+	"Whoa there, speed racer! Let's take a breather and try again in a bit.",
+	"Oops, looks like you've hit the fun limit! Come back later for more.",
+	"Too much of a good thing, eh? Let's pace ourselves and try again soon.",
+	"Looks like you've reached the end of the line! Please insert more tokens to continue.",
+	"Hold your horses! You've exceeded the maximum number of requests. Try again later, cowboy!",
+	"Slow down, tiger! You've hit the rate limit. Take a catnap and come back refreshed.",
+	"Congratulations, you've achieved the rate limit! Your prize is waiting... just not right now.",
+	"Uh-oh, you've tripped the alarm! The rate limit police are on their way. Lay low for a while.",
+	"Looks like you've had too much fun for now. Come back when the coast is clear!",
+	"You've reached the end of the all-you-can-request buffet. Time to let your requests digest!",
+	"Ooh, sorry! You've hit the rate limit wall. Don't worry, it's not permanent. Try again later!",
+	"Too many requests, too little time! Your request quota will refresh soon. Hang in there!",
+	"Whoa, easy there! You've reached the request limit. Time to take a break and smell the roses.",
+	"Well, well, well... if it isn't the rate limit! Your persistence is admirable, but let's take a breather.",
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -23,6 +44,12 @@ func main() {
 }
 
 func sendToWebhook(w http.ResponseWriter, r *http.Request) {
+	if time.Since(lastRequest) < rateLimit {
+		randomString := rateLimitResponse[rand.Intn(len(rateLimitResponse))]
+		http.Error(w, randomString, http.StatusTooManyRequests)
+		return
+	}
+	lastRequest = time.Now()
 	path := r.URL.Path
 	text := r.URL.Query().Get("text")
 	ping := r.URL.Query().Get("ping")
